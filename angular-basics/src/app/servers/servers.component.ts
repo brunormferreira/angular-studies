8032222718c9d2397dc03abf967ad02938cf214b
brunormferreira <brunormferreira@hotmail.com>
2019-09-17 15:23:00 -0300

    add: example input with two-way-data-binding

--- angular-basics/src/app/servers/servers.component.ts
+++ angular-basics/src/app/servers/servers.component.ts
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = "";
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
