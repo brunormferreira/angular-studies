11dd65e73a49a14d9d2bbb2548b05cb15033b843
brunormferreira <brunormferreira@hotmail.com>
2019-09-17 13:37:02 -0300

    add: start component

--- angular-basics/src/app/server/server.component.ts
+++ angular-basics/src/app/server/server.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
