fb1ed5024e62a6b7d78f71a5148a9792d5be9552
brunormferreira <brunormferreira@hotmail.com>
2019-09-17 14:04:43 -0300

    add: understanding component selector

--- angular-basics/src/app/servers/servers.component.ts
+++ angular-basics/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   selector: 'app-servers',
   template: `
     <app-server></app-server>
