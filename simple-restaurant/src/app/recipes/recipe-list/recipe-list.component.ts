4ebbeb0dabc364a2c14a7b93acd31129e14e72f9
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-18 13:58:37 -0300

    add: outputting a List of Recipes with *ngFor

--- simple-restaurant/src/app/recipes/recipe-list/recipe-list.component.ts
+++ simple-restaurant/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('A Test Recipe', 'This is a simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
