047886e3a51fa5fd764b1bc3981282ba68e4e95e
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-18 13:41:34 -0300

    add: content to the Recipes Components

--- simple-restaurant/src/app/recipes/recipe-list/recipe-list.component.ts
+++ simple-restaurant/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
 
   constructor() { }
 
