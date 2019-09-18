09192b40a0b33311570e9161cebc26c7bb208fc0
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-18 13:25:12 -0300

    add: creating a 'Recipe' Model

--- simple-restaurant/src/app/recipes/recipe-list/recipe-list.component.ts
+++ simple-restaurant/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes = [];
 
   constructor() { }
 
