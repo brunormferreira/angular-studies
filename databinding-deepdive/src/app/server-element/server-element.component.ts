31c7f3e6f47b39719b632751f13645f77fac2d94
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-19 10:10:28 -0300

    add: Binding to Custom Events
    used output()to emit data and EventEmitter

--- databinding-deepdive/src/app/server-element/server-element.component.ts
+++ databinding-deepdive/src/app/server-element/server-element.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  // tslint:disable-next-line: no-input-rename
   @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
