ed1babd34f3cb545add9ba4fe9a61dab6ef7f6aa
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-19 10:12:53 -0300

    add: assigning an Alias to Custom Events

--- databinding-deepdive/src/app/cockpit/cockpit.component.ts
+++ databinding-deepdive/src/app/cockpit/cockpit.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // tslint:disable-next-line: no-output-rename
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
 
