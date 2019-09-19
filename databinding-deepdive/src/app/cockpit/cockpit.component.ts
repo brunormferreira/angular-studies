9010ddf5895b7778ee0d75ed74707b1161a1f355
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-19 09:32:12 -0300

    add: Binding to Custom Properties
    with Decorator @Input() to share data to parents components

--- databinding-deepdive/src/app/cockpit/cockpit.component.ts
+++ databinding-deepdive/src/app/cockpit/cockpit.component.ts
@@ -15,18 +15,18 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
   }
 }
