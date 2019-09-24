e47032c3a1247ebec23e7ab44430365345320ac5
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-24 15:08:04 -0300

    add: Passing Query Parameters and Fragments

--- routing-deepdive/src/app/home/home.component.ts
+++ routing-deepdive/src/app/home/home.component.ts
@@ -12,8 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     // complex calculation
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 }
