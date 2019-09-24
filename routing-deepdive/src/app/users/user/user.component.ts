a65b8bf8944435f35150923e67ef6241c9d65341
Bruno Ramires <brunormferreira@hotmail.com>
2019-09-24 14:15:34 -0300

    add: Fetching Route Parameters Reactively
    the subscribe code only runs if the params changed

--- routing-deepdive/src/app/users/user/user.component.ts
+++ routing-deepdive/src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -16,5 +17,12 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 }
