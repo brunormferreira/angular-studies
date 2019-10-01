32b84e335ee1b8e48117d4fe47e7cae5c5078947
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-01 12:00:02 -0300

    add: Passing Static Data to a Route
    if not changes errorMessage uses snapshot is correct or otherwise with subs

--- routing-deepdive/src/app/app-routing.module.ts
+++ routing-deepdive/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -25,7 +26,8 @@ const appRoutes: Routes = [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
   ] },
-  { path: 'not-found', component: PageNotFoundComponent },
+  // { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
   { path: '**', redirectTo: '/not-found'}
 ];
 
