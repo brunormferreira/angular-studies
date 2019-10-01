113318fb711958c190c12a4da920d6823640952a
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-01 15:16:32 -0300

    add: Configuring Route Parameters
    only NOW its really ok when pass URL mannually to the URL link --- last commit its not working when pass the /1 or /0 ---

--- simple-restaurant/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ simple-restaurant/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
   }
 
   onAddToShoppingList() {
--- simple-restaurant/src/app/recipes/recipe.service.ts
+++ simple-restaurant/src/app/recipes/recipe.service.ts
@@ -35,6 +35,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
