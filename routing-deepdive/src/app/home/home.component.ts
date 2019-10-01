d1a2070c53b043178bd0b9419370d9d9249a800d
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-01 10:50:50 -0300

    add: Using Fake Auth Service

--- routing-deepdive/src/app/home/home.component.ts
+++ routing-deepdive/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -15,5 +16,13 @@ export class HomeComponent implements OnInit {
   onLoadServer(id: number) {
     // complex calculation
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
