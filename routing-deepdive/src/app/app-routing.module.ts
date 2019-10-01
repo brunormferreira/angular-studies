5255d14efc8a163276419b4fe79cb1ebb5b565de
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-01 10:38:32 -0300

    add: Protecting Child(Nested) Routes with canActivateChild

--- routing-deepdive/src/app/app-routing.module.ts
+++ routing-deepdive/src/app/app-routing.module.ts
@@ -15,7 +15,12 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent }
   ] },
-  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
+  {
+    path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ] },
--- routing-deepdive/src/app/auth-guard.service.ts
+++ routing-deepdive/src/app/auth-guard.service.ts
@@ -2,7 +2,8 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Router
+  Router,
+  CanActivateChild
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
@@ -10,7 +11,7 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot,
@@ -26,4 +27,9 @@ export class AuthGuard implements CanActivate {
         }
       );
   }
+
+  canActivateChild(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
+      return this.canActivate(route, state);
+    }
 }
