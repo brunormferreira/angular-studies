61024cd38b4b2ff7b46a929eb88d25a062eab7c6
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-01 13:43:17 -0300

    add: Understanding Location Strategies '#'

--- routing-deepdive/src/app/app-routing.module.ts
+++ routing-deepdive/src/app/app-routing.module.ts
@@ -34,6 +34,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
+    // RouterModule.forRoot(appRoutes, {useHash: true})
     RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
