53bc2872312d96464763b3b4af6e571e9cea5264
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-01 14:08:49 -0300

    add/returnProject: Setting Up Routes
    with NO navigation by clicks yet

--- simple-restaurant/src/app/app.module.ts
+++ simple-restaurant/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +29,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
