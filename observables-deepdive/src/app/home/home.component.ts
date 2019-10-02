e79a2bc9be7a6c9d598b359540cd72b434017519
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-02 11:41:51 -0300

    add: Understanding the Core of Observables

--- observables-deepdive/src/app/home/home.component.ts
+++ observables-deepdive/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
