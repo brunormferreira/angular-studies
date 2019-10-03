fd0739a5bf902f1753bd6042bb0db24a1e21c788
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-03 14:23:38 -0300

    add: Handling Radio Buttons

--- forms-deepdive/src/app/app.component.ts
+++ forms-deepdive/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f', {static: false}) signupForm: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
