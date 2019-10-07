c1cfa55e39529647c2187d9b55e2fc878ea2eed6
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-07 15:30:30 -0300

    add: Updating existing Items

--- simple-restaurant/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ simple-restaurant/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -37,7 +37,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy() {
