1d7b5f15b4839cd53f4dbe1263a68e43b796ba06
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-08 09:30:54 -0300

    Allowing the Deletion of Shopping List Items

--- simple-restaurant/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ simple-restaurant/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -51,6 +51,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
