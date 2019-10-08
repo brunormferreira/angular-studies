85759d9fe354e13adace9e80110e48b98678b401
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-08 10:35:49 -0300

    add: Syncing HTML with the Form

--- simple-restaurant/src/app/app.module.ts
+++ simple-restaurant/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -33,6 +33,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
--- simple-restaurant/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ simple-restaurant/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -27,6 +27,10 @@ export class RecipeEditComponent implements OnInit {
       );
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
