7f960c0f3f9c3689dcb6305df53d6abcae4da6c2
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-08 10:50:42 -0300

    add: Adding Ingredient COntrols to a FormArray

--- simple-restaurant/src/app/recipes/recipe.service.ts
+++ simple-restaurant/src/app/recipes/recipe.service.ts
@@ -22,7 +22,7 @@ export class RecipeService {
       'What else you need to say?',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
       [
-        new Ingredient('Buns', 2),
+        new Ingredient('Onion', 2),
         new Ingredient('Meat', 1)
       ]
     )
