68836d07756ddb3a45f4be79592cdd83f6985ae6
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-09 13:34:00 -0300

    add: Understading the 'async' Pipe

--- pipes-deepdive/src/app/app.component.ts
+++ pipes-deepdive/src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   servers = [
     {
       instanceType: 'medium',
