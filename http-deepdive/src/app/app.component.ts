dae4f8c3127670be287c8d7bbbd5eb6767f996c7
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-09 14:43:28 -0300

    add: GET data

--- http-deepdive/src/app/app.component.ts
+++ http-deepdive/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -26,9 +28,18 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get('https://http-angular-demo-1bb5e.firebaseio.com/posts.json')
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
