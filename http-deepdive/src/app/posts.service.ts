3d759addbbf4da28df6e846dfbeac2f8c210070a
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-10 14:45:42 -0300

    add: Using the catchError Operator
    consider using catchError if you have some gerenic error handling task

--- http-deepdive/src/app/posts.service.ts
+++ http-deepdive/src/app/posts.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -40,6 +40,10 @@ export class PostsService {
           }
         }
         return postsArray;
+      }),
+      catchError(errorRes => {
+        // Send to analytics server
+        return throwError(errorRes);
       })
     );
   }
