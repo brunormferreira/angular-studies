4da9241d0d96a60071fe6a62f144d1f6d89cc5fd
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-10 15:17:33 -0300

    add: Setting Headers

--- http-deepdive/src/app/posts.service.ts
+++ http-deepdive/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
@@ -29,7 +29,10 @@ export class PostsService {
   fetchPosts() {
     return this.http
       .get<{ [key: string]: Post }>(
-        'https://http-angular-demo-1bb5e.firebaseio.com/posts.json'
+        'https://http-angular-demo-1bb5e.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+        }
       )
       .pipe(
         map(responseData => {
