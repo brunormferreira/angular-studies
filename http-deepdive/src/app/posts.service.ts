8aea3809e59fc61746b17f51cd8aa2a1fc1521da
brunormferreira <brunormferreira@hotmail.com>
2019-10-11 12:20:58 -0300

    add: Observing Different Types of Response

--- http-deepdive/src/app/posts.service.ts
+++ http-deepdive/src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
 
 import { Post } from './post.model';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
@@ -16,7 +16,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://http-angular-demo-1bb5e.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       )
       .subscribe(responseData => {
         console.log(responseData);
@@ -57,6 +60,19 @@ export class PostsService {
 
   deletePosts() {
     return this.http
-      .delete('https://http-angular-demo-1bb5e.firebaseio.com/posts.json');
+      .delete('https://http-angular-demo-1bb5e.firebaseio.com/posts.json', {
+        observe: 'events'
+      })
+      .pipe(
+        tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
