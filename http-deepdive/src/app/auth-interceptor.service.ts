8f2ca5bff200da456de59a8f7b7c1497bb18dc0a
brunormferreira <brunormferreira@hotmail.com>
2019-10-11 14:01:23 -0300

    add: Response Interceptors

--- http-deepdive/src/app/auth-interceptor.service.ts
+++ http-deepdive/src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEventType } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -7,6 +8,14 @@ export class AuthInterceptorService implements HttpInterceptor {
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz')
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data: ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
