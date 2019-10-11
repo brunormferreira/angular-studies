0917006612432002b3009deddc2cdaac0490e081
brunormferreira <brunormferreira@hotmail.com>
2019-10-11 13:21:36 -0300

    add: Introducing Interceptors
    Being used to: portuguese => Isso nos possibilita fazer uma imensidão de coisas, um exemplo seria configurar uma autenticação via token em qualquer requisição, ou de repente adicionar headers personalizados que nossa aplicação possa necessitar, e até tratar respostas antes de terminar a requisição. As possibilidades são inúmeras.

--- http-deepdive/src/app/app.module.ts
+++ http-deepdive/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ http-deepdive/src/app/auth-interceptor.service.ts
@@ -0,0 +1,8 @@
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log('Request is on its way');
+    return next.handle(req);
+  }
+}
