1cb45b0957fcf40ae1613659a9c3029d607cf92c
brunormferreira <brunormferreira@hotmail.com>
2019-10-11 13:43:30 -0300

    add: Manipulating Request Objects

--- http-deepdive/src/app/auth-interceptor.service.ts
+++ http-deepdive/src/app/auth-interceptor.service.ts
@@ -3,6 +3,10 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log('Request is on its way');
-    return next.handle(req);
+    console.log(req.url);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest);
   }
 }
