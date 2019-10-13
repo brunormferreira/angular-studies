fbec17c64a04fddb6a32aa0dde765308cf0d591e
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-13 18:49:54 -0300

    fix: imports detail

--- simple-restaurant/src/app/shared/data-storage.service.ts
+++ simple-restaurant/src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@nangular/commom/http';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   constructor(private http: HttpClient) {}
