1ce82ebcd5b9f6742ac320793a6a7fca1df871ab
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-14 15:16:42 -0300

    add: Preparing the SignUp Request

---
+++ simple-restaurant/src/app/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDNtgBZMQxI9IypHgya4Cn4KdgXiDmWJWE',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
+}
