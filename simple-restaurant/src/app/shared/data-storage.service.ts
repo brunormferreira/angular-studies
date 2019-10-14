12f400e93d976c27e0cab041ea45a2ed2a316f31
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-13 22:23:01 -0300

    add: start skeleton storeRecipes function

--- simple-restaurant/src/app/shared/data-storage.service.ts
+++ simple-restaurant/src/app/shared/data-storage.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+
+  }
 }
