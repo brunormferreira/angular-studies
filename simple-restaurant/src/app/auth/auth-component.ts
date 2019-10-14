38640178c70344e7f2c8bde0fa0cd48ae5b677ce
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-14 14:25:16 -0300

    add: Handling Form Input with TD

--- simple-restaurant/src/app/auth/auth-component.ts
+++ simple-restaurant/src/app/auth/auth-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -9,5 +10,10 @@ export class AuthComponent {
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
   }
 }
