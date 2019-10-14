9ee6c7695b24b43a264d01d08f32eda0a11fefac
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-14 14:19:08 -0300

    add: Switching Between Auth Modes

--- simple-restaurant/src/app/auth/auth-component.ts
+++ simple-restaurant/src/app/auth/auth-component.ts
@@ -4,4 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-auth',
   templateUrl: './auth-component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
