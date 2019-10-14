b306c2e318987aa6f6033cdc9ec8c0c45a867269
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-14 20:39:44 -0300

    add: Sending the SignUp Request
    saving the user in the firebase database

--- simple-restaurant/src/app/auth/auth-component.ts
+++ simple-restaurant/src/app/auth/auth-component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from './auth.service';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth-component.html'
@@ -8,12 +10,30 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
   isLoginMode = true;
 
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
     form.reset();
   }
 }
