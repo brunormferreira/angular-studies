07ce6a35d4698dbaf364197cd9f152d88e26a535
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-16 10:11:47 -0300

    add: Reflecting the Auth State in the UI

--- simple-restaurant/src/app/auth/auth.component.ts
+++ simple-restaurant/src/app/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 import { AuthService, AuthResponseData } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -39,6 +40,7 @@ export class AuthComponent {
       resData => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },
       errorMessage => {
         console.log(errorMessage);
--- simple-restaurant/src/app/header/header.component.ts
+++ simple-restaurant/src/app/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService ) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      // this.isAuthenticated = !user ? false : true;
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -15,5 +30,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
