e729a268ab350aac1edbaba1e7b12962a8ad716e
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-17 11:41:21 -0300

    add: Adding Auto-Login
    after reload the page, login still persist because has implemented localStorage

--- simple-restaurant/src/app/auth/auth.service.ts
+++ simple-restaurant/src/app/auth/auth.service.ts
@@ -44,6 +44,29 @@ export class AuthService {
     );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -82,6 +105,7 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
