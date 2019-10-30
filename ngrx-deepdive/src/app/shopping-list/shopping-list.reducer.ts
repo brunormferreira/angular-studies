45c443ab87a966cba06cdd5057db990b2f36df93
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-30 15:54:09 -0300

    add: getting started with Reducers

---
+++ ngrx-deepdive/src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,11 @@
+import { Ingredient } from '../shared/ingredient.model';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action) {
+}
