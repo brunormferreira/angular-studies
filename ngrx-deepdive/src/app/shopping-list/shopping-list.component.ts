829e659009b4265e72edf02152ebf5205a781658
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-31 11:10:07 -0300

    add: Selecting State

--- ngrx-deepdive/src/app/shopping-list/shopping-list.component.ts
+++ ngrx-deepdive/src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription, Observable } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-shopping-list',
@@ -10,26 +11,28 @@ import { LoggingService } from '../logging.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
   private igChangeSub: Subscription;
 
   constructor(private shoppingListService: ShoppingListService,
-              private loggingService: LoggingService) { }
+              private loggingService: LoggingService,
+              private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
 
   ngOnInit() {
-    this.ingredients = this.shoppingListService.getIngredients();
-     this.igChangeSub = this.shoppingListService.ingredientsChanged
-      .subscribe(
-        (ingredients: Ingredient[]) => {
-          this.ingredients = ingredients;
-        }
-      );
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.shoppingListService.getIngredients();
+    //  this.igChangeSub = this.shoppingListService.ingredientsChanged
+    //   .subscribe(
+    //     (ingredients: Ingredient[]) => {
+    //       this.ingredients = ingredients;
+    //     }
+    //   );
 
     this.loggingService.printlog('Hello from ShoppingListComponent ngOnInit');
   }
 
   ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe();
+    // this.igChangeSub.unsubscribe();
   }
 
   onEditItem(index: number) {
--- ngrx-deepdive/src/app/shopping-list/store/shopping-list.reducer.ts
+++ ngrx-deepdive/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -18,5 +18,7 @@ export function shoppingListReducer(
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
+    default:
+      return state;
   }
 }
