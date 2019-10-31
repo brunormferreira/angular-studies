c8ae52627d5915ebff3557f7673046f1f98cb227
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-31 10:33:31 -0300

    add: Setting Up the NgRx Store

--- ngrx-deepdive/src/app/app.module.ts
+++ ngrx-deepdive/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { LoggingService } from './logging.service';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +19,8 @@ import { LoggingService } from './logging.service';
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer})
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
--- ngrx-deepdive/src/app/shopping-list/store/shopping-list.reducer.ts
+++ ngrx-deepdive/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,21 +1,22 @@
-import { Action } from '@ngrx/store';
-
 import { Ingredient } from '../../shared/ingredient.model';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient('Tomatoes', 10)
   ]
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action]
+        ingredients: [...state.ingredients, action.payload]
       };
   }
 }
